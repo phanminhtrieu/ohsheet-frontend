8982d9a644e221e883b275858d4b263425bec55a
Trieu Phan <phanminhtrieu04@gmail.com>
2025-10-01 21:11:27 +0800

    Hot-fix-OPS-61, fix environment.prod base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 const basePath ='';
-export const apiBasePath = 'https://https://ohsheet.vercel.app/';
+export const apiBasePath = 'https://ohsheet-bvh8daftbkb7f9ba.westus2-01.azurewebsites.net/';
 
 export const environment = {
     production: false,
     iconsPath: `${basePath}assets/icons`,
     imgPath: `${basePath}assets/images`,
     basePath: '',
-    apiUrl: 'https://https://ohsheet.vercel.app/api/frontend'
+    apiUrl: `${basePath}api/frontend`
   };
   
   export {};
