3d7df9838798a16ac836b657076aa254c87b6421
Trieu Phan <phanminhtrieu04@gmail.com>
2025-10-01 20:55:43 +0800

    develop first release

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -4,23 +4,34 @@ import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { NgFor } from '@angular/common';
 import { MenuItem } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
 
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [MenubarModule, ButtonModule, AvatarModule, NgFor],
+  imports: [
+    MenubarModule, 
+    ButtonModule, 
+    AvatarModule, 
+    NgFor,
+    DialogModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
   items: MenuItem[] = [];
+  comingSoonDialogVisible = false;
 
   ngOnInit() {
     this.items = [
       { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
-      { label: 'Features', icon: 'pi pi-star', routerLink: '/features' },
-      { label: 'About', icon: 'pi pi-info-circle', routerLink: '/about' },
+      // { label: 'Features', icon: 'pi pi-star', routerLink: '/features' },
+      // { label: 'About', icon: 'pi pi-info-circle', routerLink: '/about' },
     ];
+  }
+  
+  showComingSoonDialog() {
+    this.comingSoonDialogVisible = true
   }
 }
